1f1f7be59040a73da52b520cb0b8aba1aa1aa645
KAVYAJAIN410 <144513391+KAVYAJAIN410@users.noreply.github.com>
2024-03-25 13:42:35 +0530

    Add files via upload

---
+++ foundation.js
@@ -0,0 +1,66 @@
+// let a=1;
+// a=2;
+// console.log(a);
+// let FirstName="kavya";
+// let age=18;
+// let isMarried=true;
+// if(isMarried){
+//     console.log("hello, My name is "+FirstName+" and my age is "+age+" and I am married");
+// }
+// else{
+//     console.log("hello, My name is "+FirstName+" and my age is "+age+" and I am unmarried");
+// }
+// let sum=0;
+// for(let i=1;i<=1000;i++){
+//     sum=sum+i;
+// }
+// console.log(sum);
+// const arr=[21,22,23,32,45,46,47,39,12,26,24];
+// for(let i=0;i<11;i++){
+//     if(arr[i]%2==0){
+//        console.log(arr[i]);
+//     }
+// }
+// function sum(a,b,fnToCall){
+//     fnToCall(a+b);
+// }
+// function displayResult(a){
+// console.log("ans is ",a);
+// }
+//  a=sum(1,2,displayResult);
+//  console.log(a);
+// str="kavya jain";
+// console.log(str.replace("kavya","aryan"));
+// str=" kavya      jain";
+// console.log(str.trim());
+// let initialArray=[1,2,3];
+// let secondArray=[4,5,6];
+// let arr=initialArray.concat(secondArray);
+// console.log(1+"k") ;
+// current_date=new Date();
+// console.log(current_date.dime());
+// current_date=new Date();
+// console.log(current_date.getMinutes());
+// current_date.setYear(2021);
+// current_date.setDate(12);
+// console.log(current_date.getYear());
+//To know how much time function took to execute
+// day=new Date();
+// function sum(){
+//     sumi=0;
+//     for(i=0;i<1000000000000;i++){
+//         sumi+=i;
+//     }
+// }
+// time=day.getTime();
+// sum();
+// console.log(day.getTime()-time);
+temp={
+    a:"kavya",
+    b:"nai",
+    c:2
+}
+JSON.stringify(temp);
+console.log(temp);
+JSON.parse(temp);;
+
